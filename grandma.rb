2246fe2ac54dd442d3aa93f117b76eff85f6b4d9
Darrian Soto <ddog350@gmail.com>
2019-10-01 18:14:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,11 @@
 def speak_to_grandma(phrase)
-  sentence = "I LOVE YOU GRANDMA!"
   if phrase != phrase.upcase
     grandmas_reply = "HUH?! SPEAK UP, SONNY!"
     return grandmas_reply
   elsif phrase == phrase.upcase
     grandmas_reply = "NO, NOT SINCE 1938!"
     return grandmas_reply
-  elsif phrase == sentence
+  elsif phrase == "I LOVE YOU GRANDMA!"
     grandmas_reply = "I LOVE YOU TOO PUMPKIN!"
     return grandmas_reply
   else
