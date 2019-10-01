423b6aa2629403e8ef6c664f0053e0b637065423
Darrian Soto <ddog350@gmail.com>
2019-10-01 18:06:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,12 @@
 def speak_to_grandma(question)
     if question == question.upcase
-      puts grandmas_reply = "NO, NOT SINCE 1938!"
+      grandmas_reply = "NO, NOT SINCE 1938!"
       return grandmas_reply
     elsif question == "I LOVE YOU GRANDMA!"
-      puts grandmas_reply = "I LOVE YOU TOO PUMPKIN!"
+      grandmas_reply = "I LOVE YOU TOO PUMPKIN!"
       return grandmas_reply
     else
-      puts grandmas_reply = "HUH? SPEAK UP, SONNY!"
+      grandmas_reply = "HUH? SPEAK UP, SONNY!"
       return grandmas_reply
     end
 end
