88e89923b5c1a5c233d7a728e2a2b3959d687bbd
Darrian Soto <ddog350@gmail.com>
2019-10-01 18:11:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,13 @@
-def speak_to_grandma(question)
-    if question == question.upcase
-      grandmas_reply = "NO, NOT SINCE 1938!"
-      return grandmas_reply
-    elsif question == "I LOVE YOU GRANDMA!"
-      grandmas_reply = "I LOVE YOU TOO PUMPKIN!"
-      return grandmas_reply
-    else
-      grandmas_reply = "HUH? SPEAK UP, SONNY!"
-      return grandmas_reply
-    end
+def speak_to_grandma(phrase)
+  if phrase != phrase.upcase
+    grandmas_reply = "HUH?! SPEAK UP, SONNY!"
+    return grandmas_reply
+  elsif phrase == phrase.upcase
+    grandmas_reply = "NO, NOT SINCE 1938!"
+    return grandmas_reply
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    grandmas_reply = "I LOVE YOU TOO PUMPKIN!"
+    return grandmas_reply
+  else
+  end
 end
